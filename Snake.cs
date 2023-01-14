e59e4ed80ddc9b7d493a8ebaada0537940548c71
Andrej Atamantschuk <atamantschuk@googlemail.com>
2023-01-14 02:26:58 +0100

    Snake wird mit der Richtigen BodyLenght anzahl angezeigt

--- Snake.cs
+++ Snake.cs
@@ -11,79 +11,56 @@ namespace Snake
         public const char SNAKE_BODY = '■';
 
         public bool Alive { get; set; }
-        public int Body { get; set; }
+        public int BodyLenght { get; set; }
         public FieldPoint CurrentHeadPosition { get; set; }
-        public LinkedList<FieldPoint> PosotionsOfBody { get; set; }
+        public LinkedList<FieldPoint> PositionsOfBody { get; set; }
 
         public Snake(int snakePositionX, int snakePositionY)
         {
             Alive = true;
-            Body = 2;
+            BodyLenght = 3;
             CurrentHeadPosition = new FieldPoint(snakePositionX, snakePositionY, SNAKE_BODY);
-            PosotionsOfBody = new LinkedList<FieldPoint>();
-            PosotionsOfBody.AddFirst(CurrentHeadPosition);
+            PositionsOfBody = new LinkedList<FieldPoint>();
+            PositionsOfBody.AddFirst(CurrentHeadPosition);
             
         }
 
-        //Dictionary<ConsoleKey, FieldPoint> MoveDirection = new()
-        //{
-        //    {ConsoleKey.UpArrow, new FieldPoint(0, -1, SNAKE_BODY)},
-        //    {ConsoleKey.DownArrow, new FieldPoint(0, +1, SNAKE_BODY)},
-        //    {ConsoleKey.LeftArrow, new FieldPoint(-1, 0, SNAKE_BODY)},
-        //    {ConsoleKey.RightArrow, new FieldPoint(+1, 0, SNAKE_BODY)}
-        //};
+        Dictionary<ConsoleKey, FieldPoint> MoveDirection = new()
+        {
+            {ConsoleKey.UpArrow, new FieldPoint(0, -1, SNAKE_BODY)},
+            {ConsoleKey.DownArrow, new FieldPoint(0, +1, SNAKE_BODY)},
+            {ConsoleKey.LeftArrow, new FieldPoint(-1, 0, SNAKE_BODY)},
+            {ConsoleKey.RightArrow, new FieldPoint(+1, 0, SNAKE_BODY)}
+        };
 
         public void Move()
         {
             ConsoleKeyInfo keyInfo = Console.ReadKey(true);
 
-            if (keyInfo.Key == ConsoleKey.UpArrow)
+            if (MoveDirection.ContainsKey(keyInfo.Key))
             {
-                
-                if (Body > 1)
+                FieldPoint oldPosition = new FieldPoint(CurrentHeadPosition.X, CurrentHeadPosition.Y, SNAKE_BODY);
+
+                FieldPoint movement = MoveDirection[keyInfo.Key];
+                CurrentHeadPosition.X += movement.X;
+                CurrentHeadPosition.Y += movement.Y;
+
+                if (BodyLenght > 1)
                 {
-                    for (int i = 1; i < Body; i++)
-                    {
-                        FieldPoint oldPosition = new FieldPoint(CurrentHeadPosition.X, CurrentHeadPosition.Y, SNAKE_BODY);
-                        PosotionsOfBody.AddLast(oldPosition);
-                        if (PosotionsOfBody.Count > Body)
-                        {
-                            
-                        }
+                    PositionsOfBody.AddAfter(PositionsOfBody.First, oldPosition);
+                        
+                    while (PositionsOfBody.Count > BodyLenght)
+                    {                          
+                        PositionsOfBody.RemoveLast();   
                     }
                 }
-                CurrentHeadPosition.Y -= 1;
-                PosotionsOfBody[0] = CurrentHeadPosition;
-             
-
             }
-            if (keyInfo.Key == ConsoleKey.DownArrow)
-            {
-                CurrentHeadPosition.Y += 1;
-                PosotionsOfBody[0] = CurrentHeadPosition;
-
-            }
-            if (keyInfo.Key == ConsoleKey.LeftArrow)
-            {
-                CurrentHeadPosition.X -= 1;
-                PosotionsOfBody[0] = CurrentHeadPosition;
-
-            }
-            if (keyInfo.Key == ConsoleKey.RightArrow)
-            {
-                CurrentHeadPosition.X += 1;
-                PosotionsOfBody[0] = CurrentHeadPosition;
-
-            }
-
-
-
         }
+
         public void Draw()
         {
-            foreach (var positions in PosotionsOfBody)
+            foreach (var positions in PositionsOfBody)
             {
-                //if (PosotionsOfBody)
                 Console.SetCursorPosition(positions.X, positions.Y);
                 Console.Write(SNAKE_BODY);
             }
